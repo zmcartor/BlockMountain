7eba910b95af64eaa74edc1c3a61248191dd8d63
Zach McArtor <zmcartor@gmail.com>
2018-01-22 13:07:57 -0700

    first commit mess of files

---
+++ explorer.js
@@ -0,0 +1,77 @@
+(function() {
+    var Router = Backbone.Router.extend ({
+        routes : {
+            "" : "blockRoute",
+            "block/:blocknumber" : "blockRoute",
+            "transaction/:transhash" : "transactionRoute",
+            "address/:addr" : "addressRoute"
+        },
+    
+        web3 : undefined,
+    
+        initialize : function(opts) {
+            this.web3 = opts.web3;
+        },
+        
+        appHook : $("#apphook"),
+        
+        cleanup : function() {
+            this.appHook.empty()
+        },
+    
+        blockRoute : function(number) {
+            this.cleanup();
+            var num = number || "latest";
+            this.web3.eth.getBlock(num, false, function(error, blockObj) {
+                if (error != null){
+                    alert("error in latest block call boo");
+                    console.log(error);
+                    return;
+                }
+                
+                var latestBlock = new BlockView({model: blockObj});
+                $("#apphook").append(latestBlock.el);
+            });
+
+        }, 
+    
+        transactionRoute : function(trans) {
+            this.cleanup();
+
+        },
+    
+        addressRoute : function(addr) {
+            this.cleanup();
+            
+        }
+    });
+    
+    
+    // TODO this should just get the block from async call within router yo.
+    // play some animation.
+    
+    var BlockView = Backbone.View.extend( {                
+        initialize : function(options) {
+                this.render();
+        },
+        
+        render : function() {  
+            var tf = _.template($("#bt").html());        
+            this.$el.html(tf(this.model));
+        }
+    });
+    
+    
+    $().ready(function() {        
+        var web3 = new Web3( new Web3.providers.HttpProvider("https://mainnet.infura.io/3TntECqcqkTqyRsDoSNu"));
+        
+        if (typeof web3 == 'undefined'){
+            alert('Web3 provider failed to initialize. Nothing will work.');
+            return;
+        }        
+        
+        var router = new Router({ "web3" : web3 });
+        Backbone.history.start({"pushState" : true});
+    });
+
+}());
