7b8eded30fa029f94153ef91da064172c2a1ceb7
Zach McArtor <zmcartor@gmail.com>
2018-01-23 11:24:49 -0700

    parse timestamp

--- explorer.js
+++ explorer.js
@@ -77,8 +77,36 @@
         events: {"click #txns" : "showtxns"},
         
         render : function() {  
-            var tf = _.template($("#bt").html());        
+            var tf = _.template($("#bt").html());
+            
+            this.model.timestamp = this.convertTimestamp(this.model.timestamp);
+                    
             this.$el.html(tf(this.model));
+        },
+        
+        convertTimestamp : function(timestamp) {
+          var d = new Date(timestamp * 1000),
+        		yyyy = d.getFullYear(),
+        		mm = ('0' + (d.getMonth() + 1)).slice(-2),
+        		dd = ('0' + d.getDate()).slice(-2),
+        		hh = d.getHours(),
+        		h = hh,
+        		min = ('0' + d.getMinutes()).slice(-2),
+        		ampm = 'AM',
+        		time;
+			
+        	if (hh > 12) {
+        		h = hh - 12;
+        		ampm = 'PM';
+        	} else if (hh === 12) {
+        		h = 12;
+        		ampm = 'PM';
+        	} else if (hh == 0) {
+        		h = 12;
+        	}
+	
+        	time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+        	return time;
         },
         
         showtxns : function(event){
@@ -170,6 +198,11 @@
     // link out to addreses , block number
     
     var TransactionView = Backbone.View.extend({
+       
+        initialize : function(opts){
+            
+        },
+        
        
            
     });
